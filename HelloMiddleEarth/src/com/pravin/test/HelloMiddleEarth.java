3d07ab76a2af01095471005aaa531dda872b1738
pravin choudhary <pravin3c@gmail.com>
2018-04-05 12:22:47 +0530

    committing first commit for HelloMiddleEarth project

---
+++ HelloMiddleEarth/src/com/pravin/test/HelloMiddleEarth.java
@@ -0,0 +1,15 @@
+package com.pravin.test;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+
+		String[] s={"a","b","c"};
+		for(int i=0;i<s.length;i++){
+			
+		System.out.format("Hello, %s!\n", s[i]);
+		
+		}
+	}
+
+}
